523eaf6af8b42b60b66d34aa591d52ebccbe29f6
vad13irt <w13vadirt@gmail.com>
2022-08-06 21:13:04 +0300

    updated utils

--- tabularia/utils.py
+++ tabularia/utils.py
@@ -1,6 +1,10 @@
+import pandas as pd
 import numpy as np
 from typing import Optional
 import random
+import os
+
+from sklearn.model_selection import KFold, StratifiedKFold, GroupKFold, StratifiedGroupKFold
 
 
 def get_random_number(min_value:int=0, max_value:int=50) -> int:
@@ -22,3 +26,49 @@ def seed_everything(seed:Optional[int]=None) -> int:
     np.random.seed(seed)
     
     return seed
+
+
+def read_data_frame(path, library=pd, *args, **kwargs):
+    file_extension = os.path.splitext(path)[0][1:]
+    read_function_name = f"read_{file_extension}"
+    read_function = getattr(library, read_function_name)
+    data_frame = read_function(path, *args, **kwargs)
+    
+    return data_frame
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def create_folds(data_frame, 
+                 targets, 
+                 groups=None, 
+                 stratify=False, 
+                 return_data_frame=True,  
+                 fold_column="fold", 
+                 folds=5,
+                 **kwargs):
+
+    if groups is None:
+        if stratify:
+            cv_strategy = StratifiedKFold(n_splits=folds, **kwargs)
+        else:
+            cv_strategy = KFold(n_splits=folds, **kwargs)
+            
+        folds = cv_strategy.split(X=data_frame, y=targets)
+    else:
+        if stratify:
+            cv_strategy = StratifiedGroupKFold(n_splits=folds, **kwargs)
+        else:
+            cv_strategy = GroupKFold(n_splits=folds, **kwargs)
+            
+        folds = cv_strategy.split(X=data_frame, y=targets, groups=groups)
+    
+    if return_data_frame:
+        for fold, (train_indexes, validation_indexes) in enumerate(folds):
+            data_frame.loc[validation_indexes, fold_column] = int(fold+1)
+
+        return data_frame
+    
+    return folds
